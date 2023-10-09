de122057ec9aac2af70dade86576f61941b9472f
jbarriguete86 <104594909+jbarriguete86@users.noreply.github.com>
2023-10-09 15:29:01 -0500

    Update
    i created the first event listener and it's just telling me the information on colors from the API, I need to create a feedHtml function that take the information and populate the APP

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
+
+const colorInput = document.getElementById('input-color')
+const colorBtn = document.getElementById('get-scheme')
+const schemeEl = document.getElementById("scheme-options")
+const mainHtml = document.getElementById('color-stripes')
+const hexHtml = document.getElementById('hex-names')
 const darkModeBtn= document.getElementById("dark-mode")
+
+
+colorBtn.addEventListener('click', ()=>{
+    let colorValue = colorInput.value.substring(1)
+    let schemeValue = schemeEl.value
+    fetch(`https://www.thecolorapi.com/scheme?hex=${colorValue}&mode=${schemeValue}&count=5`)
+    .then(res => res.json())
+    .then(data => {
+        const infArr = data.colors
+        let colorArr =[]
+        for (info of infArr){
+           colorArr.push(info.name)  
+        }       
+    console.log(colorArr)
+   })})
+    
+
 
 darkModeBtn.addEventListener("click", ()=>{
     alert("you've clicked this button")
